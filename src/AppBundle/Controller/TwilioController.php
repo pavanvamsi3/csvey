14403e043e63ae0ea36686c91c78e1630f110f1b
patlola <praveen.patlola@practo.com>
2016-09-10 07:52:19 +0000

    inbound to outbound calls done

--- src/AppBundle/Controller/TwilioController.php
+++ src/AppBundle/Controller/TwilioController.php
@@ -18,13 +18,33 @@ class TwilioController extends Controller
      * @Route("/outbound", name="outbound_route")
      *
      */
-    public function indexAction(Request $request)
+    public function outboundAction(Request $request)
     {
         $txt = json_encode($this->get('request')->request->all());
         $txt1 = json_encode($this->get('request')->query->all());
-        $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+        $myfile = fopen("newfile.txt", "w");
         fwrite($myfile, $txt.$txt1);
         fclose($myfile);
+        $response = new Twiml();
+        $response->say('Hello Patlola');
+        $response->play('https://api.twilio.com/cowbell.mp3', array("loop" => 5));
+
+        return new Response($response);
+    }
+
+    /**
+     * @Route("/inbound", name="inbound_route")
+     *
+     */
+    public function inboundAction(Request $request)
+    {
+        $queryParams = $this->get('request')->query->all();
+        if (isset($queryParams['Direction']) == "inbound" &&
+            isset($queryParams['CallerCountry'] == "IN") &&
+            isset($queryParams['From'])) {
+            $twilioCallingManager = $this->get('twilio_calling_manager');
+            $twilioCallingManager->makeOutBoundCall($queryParams['From']);
+        }
         $response = new Twiml();
         $response->say('Hello Patlola');
         $response->play('https://api.twilio.com/cowbell.mp3', array("loop" => 5));
