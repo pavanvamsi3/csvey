e0e0c645b30aa4e36b26a57a63a6fa2e655b517c
Rahul Gupta <rahul.g@practo.com>
2016-09-10 17:51:00 +0000

    changed coee

--- src/AppBundle/Manager/UserSurveyManager.php
+++ src/AppBundle/Manager/UserSurveyManager.php
@@ -53,18 +53,18 @@ class UserSurveyManager
             $userSurvey->setSurveyId($survey);
             $userSurvey->setUserId($user);
             $balance = $user->getBalance();
-            if ($balance == 0 || $balance == null) {
+            if ($balance === null) {
                 $user->setBalance(0);
             } else {
                 $user->setBalance($balance + 2);
             }
-
             $this->em->persist($userSurvey);
             $this->em->persist($user);
             $this->em->flush();
         }
 
         return $userSurvey;
+
     }
 
     /**
