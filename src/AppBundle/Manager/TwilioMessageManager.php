faf30393b2dedd6ca4230b61e452ecb42ea2f7cd
patlola <praveen.patlola@practo.com>
2016-09-10 12:35:00 +0000

    spliting say

--- src/AppBundle/Manager/TwilioMessageManager.php
+++ src/AppBundle/Manager/TwilioMessageManager.php
@@ -25,11 +25,11 @@ class TwilioMessageManager
         $response->say('Welcome to Csvey!', array("language" => "en-IN"));
         $response->say('Answer, Listen and Earn!', array("language" => "en-IN"));
         $response->gather(array("action" => "/homemessagehandler", "timeout"=>5,
-            "method"=> "GET"))->say("Press 1 for a survey question
-                                    Press 2 for a health tips
-                                    Press 3 for a news updates
-                                    Press 4 for your current csvey balance
-                                    Press 5 to repeat the menu",
+            "method"=> "GET"))->say("Press 1 for a survey question.", array("language" => "en-IN"))
+                                    ->say("Press 2 for a health tips.")
+                                    ->say("Press 3 for a news updates.")
+                                    ->say("Press 4 for your current csvey balance.")
+                                    ->say("Press 5 to repeat the menu",
                                     array("language" => "en-IN"));
         $response->redirect('/outbound', array("method"=>"GET"));
 
